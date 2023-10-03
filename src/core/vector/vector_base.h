dd7f61901cc313236c505032f10426124a901915
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2023-10-03 21:41:17 +0300

    initial implementation of the vector class

--- src/core/vector/vector_base.h
+++ src/core/vector/vector_base.h
@@ -5,12 +5,72 @@
 
 PE_BEGIN
 
-template <typename T, int DIM>
+
+
+template <typename T, uint32_t DIM, uint32_t ALIGNMENT=sizeof(T)>
 class VectorBase {
 
-using value_type = T;
-using dimension = DIM;
+  using value_type = T;
+  using dimension = DIM;
+  using alignment = ALIGNMENT;
 
+  vector_t() = default;
+  
+  PE_HOST_DEVICE vector_t( T value ) {
+    PE_UNROLL
+    for (uint32_t i = 0; i < DIM; ++i) {
+      (*this)[i] = value;
+    }
+  }
+
+  PE_HOST_DEVICE vector_t( T* values ) {
+    PE_UNROLL
+    for (uint32_t i = 0; i < DIM; ++i) {
+      (*this)[i] = values[i];
+    }
+  }
+
+  template <typename U, uint32_t S, uint32_t A>
+  PE_HOST_DEVICE vector_t(const vector_t<U,S,A> &other) {
+    PE_UNROLL
+    for (uint32_t i = 0; i < DIM; ++i) {
+      (*this)[i] = i < M ? (T) other[i] : (T)0;
+    }
+  }
+
+  PE_HOST_DEVICE static constexpr vector_t<T, N> zeros() {
+    return vector_t<T, N>(0);
+  }
+
+  PE_HOST_DEVICE static constexpr vector_t<T, N> ones() {
+    return vector_t<T, N>(1);
+  }
+
+  // Access Methods
+
+  PE_HOST_DEVICE T& operator[](uint32_t index) {
+    return ((T*) this)[index];
+  }
+
+  PE_HOST_DEVICE const T& operator[](uint32_t index) {
+    return ((T*) this)[index];
+  }
+
+  PE_HOST_DEVICE T& operator()(uint32_t index) {
+    return ((T*) this)[index];
+  }
+
+  PE_HOST_DEVICE const T& operator()(uint32_t index) {
+    return ((T*) this)[index];
+  }
+
+  PE_HOST_DEVICE T* data() {
+    return (T*) this;
+  }
+
+  PE_HOST_DEVICE const T* data() const {
+    return (const T*) this;
+  }
 
 };
 
