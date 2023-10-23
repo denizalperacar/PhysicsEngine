278739bb47c2513d5977acf9688e60b635d07a62
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2023-10-23 17:30:55 +0300

    changed M to R and N to C for readability

--- src/core/linear_transformations/matrix_base.h
+++ src/core/linear_transformations/matrix_base.h
@@ -11,7 +11,7 @@ template <typename T, uint32_t R, uint32_t C, typename... Ts>
 using enable_if_all_vectors_t = std::enable_if_t<sizeof...(Ts) == R && conjunction<std::is_same<Ts, vector_t<T, C>>...>::value>;
 
 // here M defines a number of col and N defines a number of columns
-template <typename T, uint32_t M, uint32_t N>
+template <typename T, uint32_t R, uint32_t C>
 struct matrix_t {
 
   // let default constructor create empty matrix
@@ -20,77 +20,77 @@ struct matrix_t {
   // fill the matrix diagonal with a single value
   PE_HOST_DEVICE matrix_t(T value) {
     PE_UNROLL
-    for (uint32_t i = 0; i < M; i++) {
+    for (uint32_t i = 0; i < R; i++) {
       PE_UNROLL
-      for (uint32_t j = 0; j < N; j++) {
+      for (uint32_t j = 0; j < C; j++) {
         col[ i ][ j ] = (i == j) ? value : (T) 0;
       }
     }
   }
 
-  PE_HOST_DEVICE matrix_t(const matrix_t<T, M, N>& other) {
+  PE_HOST_DEVICE matrix_t(const matrix_t<T, R, C>& other) {
     PE_UNROLL
-    for (uint32_t i = 0; i < N; i++) {
+    for (uint32_t i = 0; i < C; i++) {
       col[ i ] = other.col[ i ];
     }
   }
 
-  PE_HOST_DEVICE static constexpr matrix_t<T, M, N> identity() {
-    return matrix_t<T, M, N>( (T)1 );
+  PE_HOST_DEVICE static constexpr matrix_t<T, R, C> identity() {
+    return matrix_t<T, R, C>( (T)1 );
   }
 
-  PE_HOST_DEVICE static constexpr matrix_t<T, M, N> zero() {
-    return matrix_t<T, M, N>( (T)0 );
+  PE_HOST_DEVICE static constexpr matrix_t<T, R, C> zero() {
+    return matrix_t<T, R, C>( (T)0 );
   }
 
   template <size_t A>
-  PE_HOST_DEVICE matrix_t<T, M, N>(const vector_t<T, N, A>& vec) {
+  PE_HOST_DEVICE matrix_t<T, R, C>(const vector_t<T, C, A>& vec) {
     PE_UNROLL
-    for (uint32_t i = 0; i < M; i++) {
+    for (uint32_t i = 0; i < R; i++) {
       col[ i ] = vec;
     }
   }
 
-  template<typename... Ts, typename = enable_if_size_and_type_match_t<M*N, T, Ts...>>
+  template<typename... Ts, typename = enable_if_size_and_type_match_t<R*C, T, Ts...>>
   PE_HOST_DEVICE matrix_t(Ts... coeffs) : data_{ coeffs... } {}
 
   PE_HOST_DEVICE matrix_t(const T* d) {
     PE_UNROLL
-    for (uint32_t i = 0; i < M; i++) {
+    for (uint32_t i = 0; i < R; i++) {
       PE_UNROLL
-      for (uint32_t j = 0; j < N; j++) {
+      for (uint32_t j = 0; j < C; j++) {
         col[ i ][ j ] = *(d++);
       }
     }
   }
 
   template <size_t A>
-  PE_HOST_DEVICE matrix_t<T, M, N>(
-      const vector_t<T, M, A>& vec1, 
-      const vector_t<T, M, A>& vec2 ) {
-    static_assert(N == 2, "matrix_t<T, M, N>(const vector_t<T, N, A>& vec1, const vector_t<T, N, A>& vec2) requires N == 2");
+  PE_HOST_DEVICE matrix_t<T, R, C>(
+      const vector_t<T, R, A>& vec1, 
+      const vector_t<T, R, A>& vec2 ) {
+    static_assert(C == 2, "matrix_t<T, R, C>(const vector_t<T, C, A>& vec1, const vector_t<T, C, A>& vec2) requires C == 2");
     col[ 0 ] = vec1; 
     col[ 1 ] = vec2;
   }
 
     template <size_t A>
-  PE_HOST_DEVICE matrix_t<T, M, N>(
-      const vector_t<T, M, A>& vec1, 
-      const vector_t<T, M, A>& vec2, 
-      const vector_t<T, M, A>& vec3 ) {
-    static_assert(N == 3, "matrix_t<T, M, N>(const vector_t<T, N, A>& vec1, const vector_t<T, N, A>& vec2) requires N == 3");
+  PE_HOST_DEVICE matrix_t<T, R, C>(
+      const vector_t<T, R, A>& vec1, 
+      const vector_t<T, R, A>& vec2, 
+      const vector_t<T, R, A>& vec3 ) {
+    static_assert(C == 3, "matrix_t<T, R, C>(const vector_t<T, C, A>& vec1, const vector_t<T, C, A>& vec2) requires C == 3");
     col[ 0 ] = vec1; 
     col[ 1 ] = vec2;
     col[ 2 ] = vec3;
   }
 
     template <size_t A>
-  PE_HOST_DEVICE matrix_t<T, M, N>(
-      const vector_t<T, M, A>& vec1, 
-      const vector_t<T, M, A>& vec2,
-      const vector_t<T, M, A>& vec3, 
-      const vector_t<T, M, A>& vec4 ) {
-    static_assert(N == 4, "matrix_t<T, M, N>(const vector_t<T, N, A>& vec1, const vector_t<T, N, A>& vec2) requires N == 4");
+  PE_HOST_DEVICE matrix_t<T, R, C>(
+      const vector_t<T, R, A>& vec1, 
+      const vector_t<T, R, A>& vec2,
+      const vector_t<T, R, A>& vec3, 
+      const vector_t<T, R, A>& vec4 ) {
+    static_assert(C == 4, "matrix_t<T, R, C>(const vector_t<T, C, A>& vec1, const vector_t<T, C, A>& vec2) requires C == 4");
     col[ 0 ] = vec1; 
     col[ 1 ] = vec2;
     col[ 2 ] = vec3;
@@ -98,8 +98,8 @@ struct matrix_t {
   }
 
   // stacks vectors one by one on top of each other
-  template<typename... Ts, typename = enable_if_all_vectors_t<T, M, N, Ts...>>
-  PE_HOST_DEVICE matrix_t<T, M, N> stack(Ts... vectors) {
+  template<typename... Ts, typename = enable_if_all_vectors_t<T, R, C, Ts...>>
+  PE_HOST_DEVICE matrix_t<T, R, C> stack(Ts... vectors) {
     col = { vectors()... };
     return *this;
   }
@@ -107,9 +107,9 @@ struct matrix_t {
   template <typename W, uint32_t U, uint32_t V>
   PE_HOST_DEVICE matrix_t(const matrix_t<W, U, V>& other) {
     PE_UNROLL
-    for (uint32_t i = 0; i < M; i++) {
+    for (uint32_t i = 0; i < R; i++) {
       PE_UNROLL
-      for (uint32_t j = 0; j < N; j++) {
+      for (uint32_t j = 0; j < C; j++) {
         col[ i ][ j ] = (i < U && j < V) ? other.col[ i ][ j ] : (T) 0;
       }
     }
@@ -117,12 +117,12 @@ struct matrix_t {
 
 
   // matrix vector multiplication
-  PE_HOST_DEVICE vector_t<T, M> operator*(const vector_t<T, N>& vec) const {
-    vector_t<T, M> result((T)0.0);
+  PE_HOST_DEVICE vector_t<T, R> operator*(const vector_t<T, C>& vec) const {
+    vector_t<T, R> result((T)0.0);
     PE_UNROLL
-    for (uint32_t i = 0; i < N; i++) {
+    for (uint32_t i = 0; i < C; i++) {
       PE_UNROLL
-      for (uint32_t j = 0; j < M; j++) {
+      for (uint32_t j = 0; j < R; j++) {
         result[j] += col[i][j] * vec[i];
       }
     }
@@ -131,8 +131,8 @@ struct matrix_t {
   
   // matrix matrix multiplication
   template <uint32_t P>
-  PE_HOST_DEVICE matrix_t<T, M, P> operator*(const matrix_t<T, N, P> & other) {
-    matrix_t<T, M, P> result(0);
+  PE_HOST_DEVICE matrix_t<T, R, P> operator*(const matrix_t<T, C, P> & other) {
+    matrix_t<T, R, P> result(0);
     PE_UNROLL
     for (uint32_t i = 0; i < P; i++) {
       result.col[ i ] = (*this) * other[ i ];
@@ -140,16 +140,16 @@ struct matrix_t {
   }
 
   // return a column
-  PE_HOST_DEVICE vector_t<T, M>& operator[](uint32_t i) {
+  PE_HOST_DEVICE vector_t<T, R>& operator[](uint32_t i) {
     return col[ i ];
   }
 
-  PE_HOST_DEVICE const vector_t<T, M>& operator[](uint32_t i) const {
+  PE_HOST_DEVICE const vector_t<T, R>& operator[](uint32_t i) const {
     return col[ i ];
   }
 
-	PE_HOST_DEVICE vector_t<T, M>& at(uint32_t idx) { return col[idx]; }
-	PE_HOST_DEVICE vector_t<T, M> at(uint32_t idx) const { return col[idx]; }
+	PE_HOST_DEVICE vector_t<T, R>& at(uint32_t idx) { return col[idx]; }
+	PE_HOST_DEVICE vector_t<T, R> at(uint32_t idx) const { return col[idx]; }
 
   PE_HOST_DEVICE T* data() {
     return data_;
@@ -168,8 +168,8 @@ struct matrix_t {
   }
 
   union {
-    vector_t<T, M> col[ N ];
-    T data_[ M * N ];
+    vector_t<T, R> col[ C ];
+    T data_[ R * C ];
   };
 
 };
