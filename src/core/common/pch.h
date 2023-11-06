9c70989f3278a3ef358b835723a886e1840dee0a
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2023-11-07 00:37:25 +0300

    added some default colors to the project

--- src/core/common/pch.h
+++ src/core/common/pch.h
@@ -9,14 +9,29 @@
 #include "../linear_transformations/homogeneous_transformation_matrix.h"
 #include "../coordinate_frame/global_frame.h"
 
-// define library specific types 
-
+// define project specific types 
 PE_BEGIN
 
 template <typename T, size_t A=sizeof(T)>
 using color_t = vector_t<T, 4, A>; 
 
+#define color(name, r, g, b, a) \
+template <typename T, size_t A> \
+constexpr color_t<T, A> name = color_t<T, A>{r, g, b, a};
 
+color(BLACK, 0, 0, 0, 1)
+color(WHITE, 1, 1, 1, 1)
+color(RED, 1, 0, 0, 1)
+color(GREEN, 0, 1, 0, 1)
+color(BLUE, 0, 0, 1, 1)
+color(YELLOW, 1, 1, 0, 1)
+color(CYAN, 0, 1, 1, 1)
+color(MAGNETA, 1, 0, 1, 1)
+color(GRAY, 0.5, 0.5, 0.5, 1)
+color(DARK_GRAY, 0.25, 0.25, 0.25, 1)
+color(LIGHT_GRAY, 0.75, 0.75, 0.75, 1)
+
+#undef color
 
 PE_END
 
