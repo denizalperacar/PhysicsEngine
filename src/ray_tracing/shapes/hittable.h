22ce63b231e1903a1ea848bd3ac0906988cca65e
Deniz A. Acar <59959106+denizalperacar@users.noreply.github.com>
2023-11-07 22:38:04 +0300

    added base shapes to the code

---
+++ src/ray_tracing/shapes/hittable.h
@@ -0,0 +1,32 @@
+#ifndef C61C8CB2_0C74_441C_8759_C05BA7E43E8B
+#define C61C8CB2_0C74_441C_8759_C05BA7E43E8B
+
+
+#include "../../core/common/pch.h"
+#include "../../core/camera/ray.h"
+
+PE_BEGIN
+
+template <typename T, size_t ALIGNMENT=sizeof(T)>
+struct hit_record {
+    vector_t<T, 3, ALIGNMENT> p;
+    vector_t<T, 3, ALIGNMENT> normal;
+    T t;
+    bool front_face;
+
+    PE_HOST_DEVICE inline void set_face_normal(const ray_t<T>& r, const vector_t<T, 3>& outward_normal) {
+        front_face = dot(r.direction, outward_normal) < 0;
+        // normal = front_face ? outward_normal : -outward_normal;
+        normal = front_face * outward_normal + (!front_face) * (-outward_normal);
+    }
+};
+
+template <typename T, size_t ALIGNMENT=sizeof(T)>
+class hittable {
+public:
+    PE_HOST_DEVICE virtual bool hit(const ray_t<T>& r, T t_min, T t_max, hit_record<T, ALIGNMENT>& rec) const = 0;
+};
+
+PE_END
+
+#endif /* C61C8CB2_0C74_441C_8759_C05BA7E43E8B */
---
+++ src/ray_tracing/shapes/sphere.h
@@ -0,0 +1,76 @@
+#ifndef BE1B4E77_37A2_4D92_A1F3_9225DDDDE9CD
+#define BE1B4E77_37A2_4D92_A1F3_9225DDDDE9CD
+
+#include "../../core/common/pch.h"
+#include "hittable.h"
+
+PE_BEGIN
+
+template <typename T, size_t A=sizeof(T)>
+class sphere : public hittable<T, A> {
+
+public:
+    PE_HOST_DEVICE sphere() = default;
+    PE_HOST_DEVICE sphere(const vector_t<T, 3, A>& center, T radius)
+        : center(center), radius(radius)
+    {}
+
+    PE_HOST_DEVICE virtual bool hit(const ray_t<T>& r, T t_min, T t_max, hit_record<T, A>& rec) const override;
+
+public:
+    vector_t<T, 3, A> center;
+    T radius;
+
+};
+
+
+template <typename T, size_t A>
+PE_HOST_DEVICE bool sphere<T, A>::hit(const ray_t<T>& r, T t_min, T t_max, hit_record<T, A>& rec) const {
+	vector_t<T, 3, A> oc = r.origin() - center;
+	auto a = length_squared(r.direction());
+	auto half_b = dot(oc, r.direction());
+	auto c = length_squared(oc) - radius * radius;
+
+	auto discriminant = half_b * half_b - a * c;
+	if (discriminant < 0) return false;
+	auto sqrt_f = sqrtf(discriminant);
+	// Find the nearest root that lies in the acceptable range.
+	auto root = (-half_b - sqrt_f) / a;
+	if (root < t_min || t_max < root) {
+		root = (-half_b + sqrt_f) / a;
+		if (root < t_min || t_max < root)
+			return false;
+	}
+
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	vec3f outward_normal = (rec.p - center) / radius;
+	rec.set_face_normal(r, outward_normal);
+
+	return true;
+}
+
+template <typename T, size_t A=sizeof(T)>
+PE_HOST_DEVICE T hit_sphere(const vector_t<T, 3, A>& center, T radius, const ray_t<T>& r) {
+  vector_t<T, 3, A> oc = r.origin() - center;
+  auto a = length_squared(r.direction());
+  auto half_b = dot(oc, r.direction());
+  auto c = length_squared(oc) - radius * radius;
+  auto discriminant = half_b * half_b - a * c;
+
+  return (discriminant < 0) * (T)-1. + (discriminant >= 0) * (-half_b - sqrt(discriminant)) / a;
+}
+
+template <typename T, size_t A=sizeof(T)>
+PE_HOST_DEVICE T hit_sphere_normal_coloring(const vector_t<T, 3, A>& center, T radius, const ray_t<T>& r) {
+  vec3f oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant < 0) * (T)-1. + (discriminant >= 0) * (-b - sqrt(discriminant)) / ((T)2.0 * a);
+}
+
+PE_END
+
+#endif /* BE1B4E77_37A2_4D92_A1F3_9225DDDDE9CD */
